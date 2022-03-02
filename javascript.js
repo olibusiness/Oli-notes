b2c1a76536910e6244336dbddbc41c71206b031d
olibusiness <oliver.business101@gmail.com>
2022-03-02 20:14:45 +1100

    first commit

---
+++ javascript.js
@@ -0,0 +1,220 @@
+const quotes = [{
+    quote: `Musk loves to read computer and rocket experiments he would often make model rockets and even fired some at school. 
+    He was bullied a lot for being different. 
+    He got his nose broken and had to get a nose job to fix it.`,
+    author: `Elon musk by Ashlee Vance`
+}, {
+    quote: `Elon and a friend rented out a huge frat house while they were in college. 
+    They would live there and every Friday through a huge party with a $5 entry fee. 
+    This would make them a lot of money since up to 500 people would show up.`,
+    author: `Elon musk by Ashlee Vance`
+}, {
+    quote: `x.com was the company that Elon started which later merged with pay pal where Elon became the CEO. 
+    Back then persuading people to trust and put their money on the internet was quite hard.`,
+    author: `Elon musk by Ashlee Vance`
+}, {
+    quote: `this sam hamptussy got me actin unwise`,
+    author: `Marley Kessler`
+}, {
+    quote: `Most of the PayPal people turned on Elon and kicked him out of his CEO role. 
+    Elon was not happy at the start of this decision but later accepted it and kept investing in the company making him the biggest shareholder. 
+    When PayPal sold to eBay Elon made $250 million dollars, 180 million after tax`,
+    author: `Elon musk by Ashlee Vance`
+}, {
+    quote: `Musk moved to LA where he got back into his passion of space. He joined a mars space club. He wanted to know more and why NASA wasn’t going to mars. 
+    He joined the board of directors and then donated $100,000 to fund some research. He bought his way in.`,
+    author: `Elon musk by Ashlee Vance`
+}, {
+    quote: `Musk went on a vacation back to south Africa (where he grew up) and while he was there, he got malaria it took him 6 months to recover he lost 45 pounds and looked like a Skelton. 
+    He famously said 
+    “that's my lesson for taking a vacation. Vacation's kill you”`,
+    author: `Elon musk by Ashlee Vance`
+}, {
+    quote: `Elon wanted to buy rockets from the Russians but their prices were to high do Elon decided to just build his own rocket company. 
+    He went and found the best engineers and tried to get them to join space x. 
+    his aim was to create cheap space flight.`,
+    author: `Elon musk by Ashlee Vance`
+}, {
+    quote: `Before the space x team had finished the falcon 1 let alone launched it Musk announced the falcon 5.
+    It was called the falcon 5 because of the 5 engines.
+    The falcon 5 was said to be able to reach the iss which would lead to big contracts for space x.`,
+    author: `Elon musk by Ashlee Vance`
+}, {
+    quote: `Space x failed the first launch of the falcon 1. So, the next year the space x team came back and attempted another launch. 
+    All was going well until the 5-minute mark where the rocket started aggressively shacking and then exploded. 
+    Everyone said that Elon had 1 maybe 2 launches till he ran out of money and would have to shut down space x.`,
+    author: `Elon musk by Ashlee Vance`
+}, {
+    quote: `The Tesla engineers had lots of clever ways to test the roadster one of which. 
+    They had to test how well it could handle low temperatures and if the batteries could survive.
+     Instead of using a fancy cooling place they got an ice cream truck put it in there and did their tests`,
+    author: `Elon musk by Ashlee Vance`
+}, {
+    quote: `Tesla replaced the CEO of tesla with Musk because the current CEO was running the cost way to high it would have cost $200,000 to make one roadster and tesla wanted to sell them for $90,000.
+    The CEO was moved to board of directors but was unhappy and left.`,
+    author: `Elon musk by Ashlee Vance`
+}, {
+    quote: `Elon and his first wife Justine wanted a divorce because Elon wasn't paying much attention to ger as he was always busy trying to run space x and tesla Justine started to post these problems on her blog.
+    She signed an agreement before the marriage that she would get nothing but later took Elon to court to change that. 
+   She got the mansion 2 million dollars in cash $80,000 a moth in child support`,
+    author: `Elon musk by Ashlee Vance`
+}, {
+    quote: `After 3 failed launches space x gad one more chance to get it right before all the funding would run out.
+    If this rocket failed it would have led to the end of space x. space x became the first ever privately funded company to reach orbit.
+    It took 6 years and 500 people.`,
+    author: `Elon musk by Ashlee Vance`
+}, {
+    quote: `Grifen who almost became a cofounder of space x was now the head of NASA. He said he would give a contact to space x for 1 billion dollars. 
+    He later tried to turn on this, but NASA employees disagreed, so SpaceX received a reward of 1.6 billion dollars in funding for 12 flights to the ISS.`,
+    author: `Elon musk by Ashlee Vance`
+}, {
+    quote: `After the retirement of the space shuttle the USA had to rely on the Russians to get people and supplies to the ISS.
+    With one person costing the USA 70 million dollars.
+  SpaceX provided a new option at a fraction of the price and in the US`,
+    author: `Elon musk by Ashlee Vance`
+}, {
+    quote: `space x has found thousands of ways to cut down on launch costs.
+    A major one is that space x uses consumer electronics instead of space grade ones.
+    It was hard to convince NASA that they work well enough, but this cut the cost down dramatically`,
+    author: `Elon musk by Ashlee Vance`              
+}, {
+    quote: `Normal combustion engines only turn about 10-20% of gasoline into propulsion the rest of the energy is lost through out the car.
+    While a model S turns about 60% of electricity into motion the rest is lost to heat.`,
+    author: `Elon musk by Ashlee Vance`              
+}, {
+    quote: `A group of Tesla engineers test drove and bought a Mercedes cls they then ripped everything out and made it all electric so they could see what a Model s would feel like.
+    When they took it out for public drives, they had to weld the exhaust pipe back on so no one would notice`,
+    author: `Elon musk by Ashlee Vance`              
+}, {
+    quote: `Tesla was having trouble converting reserves into orders so tesla started getting into trouble with money, so Elon turned 500 people into sales men to try sell the cars.
+    Elon started talking to google about his problems and google started making a deal to buy Tesla.
+    Luckily right before the deal could be made Tesla started making sales.`,
+    author: `Elon musk by Ashlee Vance`              
+}, {
+    quote: `One of the ways Elon is thinking of getting to mars is to launch a rocket to orbit then send a tanker to fuel it up for its journey to mars.
+    They will have then travel at high speeds and get to mars in around 3 months`,
+    author: `Elon musk by Ashlee Vance`              
+}, {
+    quote: `Musk is not very good with his employees he tends to get upset at people for very small things he even fires people for typos in emails or saying no to something or it can't be done. He doesn't have many friends because of this. 
+    But he just wants things done`,
+    author: `Elon musk by Ashlee Vance`  
+    //--------------------------------Money quotes no more elon--------------------------------------------
+}, {
+    quote: `No one is crazy with money we have all just lived through different experiences.
+    Most people chose to follow their experiences rather than some second-hand knowledge because that's what they believe Is right.
+    We are also raised by the time if you put two equally smart people from different times, they will have different views on stocks and money.`,
+    author: `The Psychology of money by Morgan Housel` 
+    
+}, {
+    quote: `Luck plays a big roll bill gates got very lucky he went to lakeside high school which also happened to be one of the only high schools with a computer.
+    Bill and his friends spent hours before and after school play around with it.
+    Bill once said that if he didn't go to lakeside, he probably wouldn't have made Microsoft.`,
+    author: `The Psychology of money by Morgan Housel`
+
+}, {
+    quote: `Bill gates said “success is a lousy teacher.
+    It seduces smart people into thinking they can't lose”. 
+    We tend to look at billionaires for lessons bit the more extreme the outcome the less likely you can apply its lessons to your own life. 
+   Because luck played a huge part in it.`,
+    author: `The Psychology of money by Morgan Housel`
+
+}, {
+    quote: `When will you have enough? When you get to a million why not 5 million or 10 and it keeps going of you have enough and understand when you do that's how you become happy.
+    You don't know how much you can it until you do it and throw up same with money you want more until you lose it.`,
+    author: `The Psychology of money by Morgan Housel`
+
+}, {
+    quote: `Investing is all about compounding Waren Buffet has been investing since he was 10. He is now worth 84.5 billion. 
+    If he had been normal person and started investing between 20-30 with a return of 22%, he would only be worth 11.9 million 99.9% less.
+     His skill is investing but his secret is time.`,
+    author: `The Psychology of money by Morgan Housel`
+
+}, {
+    quote: `Jesse Livermore was one of the greatest stock traders in his day. D
+    uring the market crash everyone was panicking when jesses family was in tears because they thought they lost all there money but Jesse bet against it and made the equivalent of 3 billion today. 
+    He made bigger bets got greedy and lost all his money. He later committed suicide`,
+    author: `The Psychology of money by Morgan Housel`
+
+}, {
+    quote: `“A wise old owl lived in a oak the more he saw the less he spoke the less he spoke the more he heard why aren't we all like the wise old bird”.
+    John D Rockefeller use to recite this poem when people asked him why he was so quiet.
+    He was one of the most successful people in 1870s.`,
+    author: `The Psychology of money by Morgan Housel`
+
+}, {
+    quote: `Man in the car paradox no one is impressed with your possessions as much as you are.
+    The man in the car paradox says when people see a nice or fancy car them almost never think about the person driving it.
+     Everyone pictures them self in the driver's seat.
+    The saw goes for anything nice people think it brings them respect bit people only imagine themselves driving.`,
+    author: `The Psychology of money by Morgan Housel`
+
+}, {
+    quote: `“Things that have never happened before happen all the time”. Said by standard professor Scott Sagan`,
+    author: `The Psychology of money by Morgan Housel`
+
+}, {
+    quote: `It's hard to stick to one plan or career because we change as people. Our goals hopes, views, on things change all the time.
+    It's very hard to stick to a job from 18-65 and still be happy.
+    Its better to change and be happy rather then a prisoner of the past.`,
+    author: `The Psychology of money by Morgan Housel`
+
+}, {
+    quote: `Most people are attracted by bad stuff. Tell someone everything is great and they're likely to shrug you off or call you a salesperson.
+    Tell someone there in danger and now you have their undivided attention.`,
+    author: `The Psychology of money by Morgan Housel`
+
+}, {
+    quote: `Everyone has an incomplete view of the world. But we form a complete narrative to fill the gap. 
+    Most people when confronted with something they don't understand, don't realize that they don't understand because there able to come up with an explanation based on there unique perspective.`,
+    author: `The Psychology of money by Morgan Housel`
+    //-----------------------------the unfair advantage-----------------
+}, {
+    quote: `Evan Spiegel the youngest self-made billionaire (24) and founder of snapchat came from an extremely rich family.
+    His mum and dad were very successful lawyers.
+    He grew up and had huge advantages he went to a rich private school and had $250 dollar an hour tutoring although he worked very hard, he also had an unfair advantage.`,
+    author: `Unfair advantage Ash Ali & Hasan Kubba`
+
+}, {
+    quote: `Oprah had a very hard up bringing she would switch between living with her mum dad or grandmother.
+    She went through traumatic experiences. She was sexually abused by the age of 9.
+     Although she went through all this, she was also lucky.
+    Her grandmother taught her how to read at the age of 3 and her dad would take her to talk in font of hundreds of people.`,
+    author: `Unfair advantage Ash Ali & Hasan Kubba`
+
+}, {
+    quote: `Money is a huge part of successes.
+    The world works around money after all. (seo) search engine optimisation is a big part of a business its where google pits you on the list. 
+    The higher you are the better for the business.
+    Before google the people at the top of the list were just the people who could pay the most.`,
+    author: `Unfair advantage Ash Ali & Hasan Kubba`
+
+}, {
+    quote: `Its very useful to get a job in an industry because then you can easily identify holes in the market so you can develop a product or service better.
+    Once you have the job you can understand how and why you are going to tackle these problems with people in the same field.`,
+    author: `Unfair advantage Ash Ali & Hasan Kubba`
+
+}, {
+    quote: `When running a business, it's a good idea to try the jobs your employees are doing. Them you can find ways to make their job and everything else easier. 
+    Deliveroo founder did this he quit his job and became a full-time delivery man for 9 months this helped him understand how to make the service better`,
+    author: `Unfair advantage Ash Ali & Hasan Kubba`
+
+}, {
+    quote: `Everyone thinks to be successful you have to come up with some brand-new idea but that's not the case. 
+    Most business don't come up with a ground-breaking idea. They find an idea and slightly change it and make it better “the pioneers take the arrows the settlers take the land”. 
+    You have to look at the world find problems and fix them.`,
+    author: `Unfair advantage Ash Ali & Hasan Kubba`
+}, {
+    quote: `.`,
+    author: `Zach Wellman`
+}]
+// New Quote Button
+const quoteBtn = document.getElementById('quote-btn');
+// Get Quote and author Section
+const quote = document.querySelector('.quote');
+const author = document.querySelector('.author');
+// Event Handler
+quoteBtn.addEventListener('click', () => {
+    let random = Math.floor(Math.random() * quotes.length);
+    quote.innerHTML = quotes[random].quote;
+    author.innerHTML = quotes[random].author;
+})
